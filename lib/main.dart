a1fd34a44e8eb9ef65f3b12e09ed7c37b26db321
Fitz Do Ngoc Quy <it.fitz@voltrans.org>
2022-02-15 20:51:27 +0700

    add todo model & single item. test git config

--- lib/main.dart
+++ lib/main.dart
@@ -20,82 +20,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('the whole app rebuild');
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MyInstantProvider(),
-                Consumer(
-                  builder: (context, text, child) {
-                    return MyText(
-                        'Value: ${context.watch<IndexChangeNotifier>().index}');
+      home: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text(mockTodo.title),
+                trailing: Checkbox(
+                  value: mockTodo.isCompleted,
+                  onChanged: (newValue) {
+                    // TODO:
                   },
                 ),
-              ],
-            ),
+                subtitle: Text(mockTodo.date.toString()),
+              ),
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              context.read<IndexChangeNotifier>().increment();
-            },
-            child: const Icon(Icons.add),
-          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.read<IndexChangeNotifier>().increment();
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
   }
 }
 
-// Create and using the value of the provider instantly
-// & using consumer so can testing for error purpose
-class MyInstantProvider extends StatelessWidget {
-  const MyInstantProvider({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Provider(
-      create: (_) => StringModel('_title'),
-      child: Container(
-        height: 50,
-        color: Colors.yellow,
-        child: Consumer<StringModel>(
-          builder: (_, stringModel, __) => Text(stringModel.title),
-        ),
-      ),
-    );
-
-    /// This code will throw an error
-    /// This usually happens when you are creating a provider and trying to read it immediately.
-
-    /// ```
-    ///   return Provider(
-    ///   create: (_) => StringModel('_title'),
-    ///   child: Container(
-    ///     height: 50,
-    ///     color: Colors.yellow,
-    ///     child: Text(Provider.of<StringModel>(context).title),
-    ///   ),
-    /// );
-    /// ```
-  }
-}
-
-// Testing for Consumer reduce rebuild cost
-class MyText extends StatelessWidget {
-  const MyText(this.text, {Key? key}) : super(key: key);
-  final String text;
-  @override
-  Widget build(BuildContext context) {
-    print('my text had rebuild');
-    return Text(text);
-  }
-}
+final mockTodo = Todo(
+  id: 'Todo Id',
+  title: 'Learn Provider',
+  date: DateTime.now(),
+);
--- lib/models.dart
+++ lib/models.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Todo {
+  final String id;
+  final String title;
+  final bool isCompleted;
+  final DateTime date;
+
+  Todo({
+    this.isCompleted = false,
+    required this.id,
+    required this.title,
+    required this.date,
+  });
+}
 
 class IndexChangeNotifier with ChangeNotifier {
   int _index = 0;
