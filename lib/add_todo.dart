81682bdecaeb2cecc103c2fd1ce3c8fc4153e3d6
Fitz Do Ngoc Quy <it.fitz@voltrans.org>
2022-02-16 13:17:25 +0700

    remove the default appbar

--- lib/add_todo.dart
+++ lib/add_todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'todo.dart';
 
@@ -20,57 +21,96 @@ class AddTodoPageState extends State<AddTodoPage> {
   final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Add new Todo')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                validator: (v) {
-                  if (v == null || v.isEmpty) {
-                    return 'You must enter a todo title';
-                  } else {
-                    return null;
-                  }
-                },
-                controller: _titleController,
-                decoration: InputDecoration(
-                  hintText: 'Title',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      Navigator.pop(
-                        context,
-                        Todo(
-                          title: _titleController.text,
-                          id: _titleController.text,
-                          date: DateTime.now(),
+    return Container(
+      color: ThemeData().primaryColor,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            elevation: 0,
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+            backgroundColor: Colors.transparent,
+            toolbarHeight: MediaQuery.of(context).size.height * 0.18,
+            flexibleSpace: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 0,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.arrow_back_ios),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text(
+                          'Add new Todo',
+                          style:
+                              Theme.of(context).textTheme.headline3!.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                  ),
                         ),
-                      );
-                    } else {}
-                  },
-                  child: const Text('Add'),
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      const StadiumBorder(
-                        side: BorderSide(color: Colors.transparent),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (v) {
+                      if (v == null || v.isEmpty) {
+                        return 'You must enter a todo title';
+                      } else {
+                        return null;
+                      }
+                    },
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Title',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.pop(
+                            context,
+                            Todo(
+                              title: _titleController.text,
+                              id: _titleController.text,
+                              date: DateTime.now(),
+                            ),
+                          );
+                        } else {}
+                      },
+                      child: const Text('Add'),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          const StadiumBorder(
+                            side: BorderSide(color: Colors.transparent),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_app_fitz/todo.dart';
 import 'package:todo_app_fitz/todo_tile.dart';
 
@@ -18,29 +19,58 @@ class _AppState extends State<App> {
   List<Todo> todos = [...mockTodos];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Fitz Todo App')),
-      body: SafeArea(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: todos.length,
-          itemBuilder: (context, index) => TodoTile(
-            todos[index],
-            onSwipe: (_) => setState(() => todos.removeAt(index)),
+    return Container(
+      color: ThemeData().primaryColor,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            elevation: 0,
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+            backgroundColor: Colors.transparent,
+            toolbarHeight: MediaQuery.of(context).size.height * 0.15,
+            flexibleSpace: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                elevation: 0,
+                child: ListTile(
+                  title: Text(
+                    'My Todos',
+                    style: Theme.of(context).textTheme.headline3!.copyWith(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                  ),
+                  subtitle: const Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text('3 out of 5'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          body: ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            shrinkWrap: true,
+            itemCount: todos.length,
+            itemBuilder: (context, index) => TodoTile(
+              todos[index],
+              onSwipe: (_) => setState(() => todos.removeAt(index)),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              final Todo todo = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddTodoPage()),
+              );
+              setState(() {
+                todos.add(todo);
+              });
+            },
+            child: const Icon(Icons.add),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final Todo todo = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const AddTodoPage()),
-          );
-          setState(() {
-            todos.add(todo);
-          });
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
