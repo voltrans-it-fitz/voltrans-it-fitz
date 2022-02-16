4ef6f0716ba342bb2de898b71d03dc28973fe2dc
Fitz Do Ngoc Quy <it.fitz@voltrans.org>
2022-02-16 16:52:50 +0700

    add TodoManager change notifier class

---
+++ lib/todo_manager.dart
@@ -0,0 +1,31 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
+import 'package:todo_app_fitz/todo.dart';
+
+class TodoManager extends ChangeNotifier {
+  final List<Todo> _todos;
+
+  get todos => UnmodifiableListView(_todos);
+
+  get completedTodoCount => _todos.where((e) => e.isCompleted).toList().length;
+
+  get todosLength => _todos.length;
+
+  TodoManager({required List<Todo> todos}) : _todos = todos;
+
+  toggleTodo(index) {
+    _todos[index].isCompleted = !_todos[index].isCompleted;
+    notifyListeners();
+  }
+
+  deleteTodo(index) {
+    _todos.removeAt(index);
+    notifyListeners();
+  }
+
+  addTodo(Todo todo) {
+    _todos.add(todo);
+    notifyListeners();
+  }
+}
