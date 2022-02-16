fc011a25a2271bbb0128bfb770f6037b808b38bd
Fitz Do Ngoc Quy <it.fitz@voltrans.org>
2022-02-16 16:53:03 +0700

    provide the todo manager & extract the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app_fitz/todo.dart';
+import 'package:todo_app_fitz/todo_manager.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(const FitzTodoApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TodoManager(todos: [...mockTodos]),
+      child: const FitzTodoApp(),
+    ),
+  );
 }
 
 class FitzTodoApp extends StatelessWidget {
@@ -16,8 +25,22 @@ class FitzTodoApp extends StatelessWidget {
       title: 'Fitz Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: _appBarTheme,
+        textTheme: const TextTheme(headline3: _h3Theme),
       ),
       home: const App(),
     );
   }
 }
+
+const _appBarTheme = AppBarTheme(
+
+  actionsIconTheme: IconThemeData(color: Colors.black),
+  elevation: 0,
+  systemOverlayStyle: SystemUiOverlayStyle.light,
+  backgroundColor: Colors.transparent,
+);
+const _h3Theme = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
